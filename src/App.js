4260c6c92de5523896c5047606f4758804757e74
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-03 13:55:03 -0700

    Map over title and id

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
   
   return (
     <div className="categories-container">
-      {categories.map(category => (
+      {categories.map(({ title, id }) => (
         <div className="category-container">
           <div className="background-img" />
           <div  className="category-body-container"
-          key={category.id}>
-            <h2>{category.title}</h2>
+          key={id}>
+            <h2>{title}</h2>
             <p>Shop Now</p> 
             </div>
           </div>
