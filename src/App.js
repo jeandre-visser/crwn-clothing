15b3c2e000b2ccc54ce07232dfa2790f10aaecca
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-03 13:09:32 -0700

    Creata array of categories and map over to display div of each

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
-
-
 function App() {
+  
+  const categories = [
+    { id: 1, title: "Hats" },
+    { id: 1, title: "Jackets" },
+    { id: 1, title: "Sneakers" },
+    { id: 1, title: "Womens" },
+    { id: 1, title: "Mens" },
+  ]
+  
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p> 
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p> 
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p> 
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p> 
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p> 
-        </div>
-      </div>
+      {categories.map(category => (
+        <div className="category-container">
+          {/* <img /> */}
+          <div  className="category-body-container"
+          key={category.id}>
+            <h2>{category.title}</h2>
+            <p>Shop Now</p> 
+            </div>
+          </div>
+      ))}
     </div>
   );
 }
