3efd04d16b139b351282634eadb4ee3f3006ea59
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-03 14:11:58 -0700

    Create category-item component and pass props from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './categories.styles.scss';
+import CategoryItem from './components/category-item/category-item.component';
 
 function App() {
   
@@ -33,18 +34,8 @@ function App() {
   
   return (
     <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div className="category-container">
-          <div 
-            className="background-image" 
-            style={{ backgroundImage: `url(${imageUrl})`}}  
-          />
-          <div  className="category-body-container"
-          key={id}>
-            <h2>{title}</h2>
-            <p>Shop Now</p> 
-            </div>
-          </div>
+      {categories.map((category) => (
+       <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
