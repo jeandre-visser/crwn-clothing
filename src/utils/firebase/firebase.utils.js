30c6cce21f3523d3840fbec2191bba356f9c82f4
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-05 16:34:09 -0700

    Initialize firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCy0ZmZQyPERWOny_bhiHgr-8gDxxCOeHg",
+  authDomain: "crwn-clothing-db-95bba.firebaseapp.com",
+  projectId: "crwn-clothing-db-95bba",
+  storageBucket: "crwn-clothing-db-95bba.appspot.com",
+  messagingSenderId: "500777622020",
+  appId: "1:500777622020:web:1e0967406eaa41d4b2b351"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
