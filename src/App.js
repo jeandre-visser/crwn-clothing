988af2b520baa3dbe2f8067a74d0f40c62272e6f
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-04 17:19:37 -0700

    Create more routes to test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import Home from './routes/home/home.component';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+const Shop = () => {
+  return <h1>Hello there</h1>
+}
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <h1>Navbar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navbar />}>
+        <Route index element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
+      </Route>
     </Routes>
   )
 }
