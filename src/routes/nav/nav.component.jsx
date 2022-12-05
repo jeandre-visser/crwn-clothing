e7c85dfce7734391817d9a9e355f33b7c5e781f8
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-04 17:45:44 -0700

    Import crown logo as React component

--- src/routes/nav/nav.component.jsx
+++ src/routes/nav/nav.component.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from 'react-router-dom';
 import { Fragment } from 'react';
+import './nav.styles.scss';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 const Navbar = () => {
   return (
     <Fragment>
       <div className="nav" >
         <Link to='/' className="logo-container">
-          <div>Logo</div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="links-container">
           <Link to='/shop' className="nav-links-container">
