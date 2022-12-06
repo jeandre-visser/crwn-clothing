d34ee593fb1df5a4e9980edafc38911545f02279
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-05 19:43:04 -0700

    Get the auth info when the sign in component is mounted via useEffect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,25 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from '../../utils/firebase/firebase.utils';
+
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
 
 const SignIn = () => {
+  // Run this function one time, once the SignIn component mounts
+  useEffect(async () => {
+    const response = getRedirectResult(auth);
+    console.log(response)
+  }, [])
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user)
-
-    console.log(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   }
-
+  
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In With Google</button>
+      <button onClick={signInWithGoogleRedirect}>Sign In With Google Redirect</button>
     </div>
   )
 }
