0d8dbc80af4e9164eeaa745c46144774088e5f39
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-06 15:35:09 -0700

    Add an h2

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -3,6 +3,8 @@ import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '
 
 import FormInput from '../form-input/form-input.component';
 
+import './sign-up-form.styles.scss';
+
 const defaultFormFields = {
   displayName: '',
   email: '',
@@ -53,8 +55,9 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className='"sign-up-container'>
+      <h2>Don't Have An Account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput 
           label="Display Name"
