14085da31030698bfc8965c97302f7563bf1f92e
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-06 14:20:45 -0700

    Create auth with netive provider of email and password using createUserWithEmailAndPassword

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 
@@ -52,4 +52,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   return userDocRef;
 }
 
-
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
