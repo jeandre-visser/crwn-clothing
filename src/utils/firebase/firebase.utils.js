8beba0cf342d0c267535977da036742575779590
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-05 19:21:45 -0700

    If user does not exists, create user by extracting the userAuth email and displayName then setDoc, otherwise just return the user doc ref

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -32,4 +32,26 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userSnapshot = await getDoc(userDocRef)
   console.log(userSnapshot);
   console.log(userSnapshot.exists());
+  
+  // if user data does not exist
+  // create / set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+  
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      })
+    } catch (err) {
+      console.log('error creating user', err.message)
+    }
+  }
+
+  // if user data exists
+  return userDocRef;
 }
+
+
