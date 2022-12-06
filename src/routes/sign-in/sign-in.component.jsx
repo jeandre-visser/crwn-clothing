05e2ddd5b963b24570a8cd449dfb9ca535fe69a3
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-05 20:21:55 -0700

    Just use sign in with pop up

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,25 +1,16 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from '../../utils/firebase/firebase.utils';
-
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-  // Run this function one time, once the SignIn component mounts
-  useEffect(async () => {
-    const response = getRedirectResult(auth);
-    console.log(response)
-  }, [])
-
+  
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-  }
+  };
   
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In With Google</button>
-      <button onClick={signInWithGoogleRedirect}>Sign In With Google Redirect</button>
     </div>
   )
 }
