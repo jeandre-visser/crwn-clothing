d4e14a4a661f16a3cd9b2c2530ff987977bdf323
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-05 19:23:46 -0700

    remove console logs

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -27,11 +27,8 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, 'users', userAuth.uid)
-  console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef)
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
   
   // if user data does not exist
   // create / set the document with the data from userAuth in my collection
