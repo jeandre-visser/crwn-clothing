d52f6b8578b6ff8bf16509215bd7b7c6923ab56c
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-06 15:15:18 -0700

    Create a form input component and pass into sign up form

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FormInput = ({ label, ...otherProps }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input 
+        {...otherProps}
+      />
+    </div>
+  )
+}
+
+export default FormInput
