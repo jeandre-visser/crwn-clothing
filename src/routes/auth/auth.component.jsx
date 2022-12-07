c36c30b2e9d3c73190c553d9d1a023af510698c8
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-06 17:05:55 -0700

    Remove constant

--- src/routes/auth/auth.component.jsx
+++ src/routes/auth/auth.component.jsx
@@ -1,17 +1,18 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
 const Auth = () => {
   
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
+    await createUserDocumentFromAuth(user);
   };
   
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign In With Google</button>
+      <SignInForm />
       <SignUpForm />
     </div>
   )
