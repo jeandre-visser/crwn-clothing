ebf2248c389541eb8c721a4065fb3b33612b4bf1
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-06 17:31:37 -0700

    Pass in sign in with email and password into the try catch statement

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
-import { signInWithGooglePopup , createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js';
+import { 
+  signInWithGooglePopup , createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from '../../utils/firebase/firebase.utils.js';
+
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 import { FcGoogle } from 'react-icons/fc';
@@ -31,9 +35,21 @@ const SignInForm = () => {
 
   
     try {
-      
+      const response = await signInAuthUserWithEmailAndPassword(email, password)
+      console.log(response)
+      resetFormFields();
     } catch (err) {
 
+      switch(err.code) {
+        case 'auth/wrong-password':
+          alert('Incorrect password');
+          break;
+        case 'auth/user-not-found':
+          alert('User not found');
+          break;
+        default:
+          console.log(err.message)
+      }
     }
   }
 
@@ -68,8 +84,9 @@ const SignInForm = () => {
             Sign In
           </Button>
           <Button 
-          onClick={signInWithGoogle}
-          buttonType="google"
+            type="button"
+            onClick={signInWithGoogle}
+            buttonType="google"
           >
           Sign In With <FcGoogle size={20} />
           </Button>
