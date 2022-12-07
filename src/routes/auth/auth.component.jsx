3ade61dae878d87158833294f0e257b8ca31ef81
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-06 17:30:17 -0700

    Style auth page

--- src/routes/auth/auth.component.jsx
+++ src/routes/auth/auth.component.jsx
@@ -1,17 +1,12 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
+import './auth.styles.scss';
+
 const Auth = () => {
-  
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
-  };
-  
+   
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className="auth-container">
       <SignInForm />
       <SignUpForm />
     </div>
