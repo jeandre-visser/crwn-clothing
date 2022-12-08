e9d7fb01a4b6b33ce05e625b7f5980ef3886bb50
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-07 20:59:26 -0700

    Make ProductsProvider plular and map over the products state from ProductsContext

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -2,20 +2,20 @@ import { createContext, useState } from "react";
 
 import PRODUCTS from '../shop-data.json';
 
-export const ProductContext = createContext({
+export const ProductsContext = createContext({
   products: [],
 })
 
-export const ProductProvider = ({ children }) => {
+export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState(PRODUCTS);
   
   const value = { products };
 
   return (
-    <ProductContext.Provider
-      value={}
+    <ProductsContext.Provider
+      value={value}
     >
       {children}
-    </ProductContext.Provider>
+    </ProductsContext.Provider>
   )
 }
