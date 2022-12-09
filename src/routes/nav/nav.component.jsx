7fe75a70dbe2d3a6c4694000c7a3a3bd5beb929a
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-08 20:41:46 -0700

    Place cart dropdown in nav

--- src/routes/nav/nav.component.jsx
+++ src/routes/nav/nav.component.jsx
@@ -4,6 +4,7 @@ import './nav.styles.scss';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -33,6 +34,7 @@ const Navbar = () => {
           )}
           <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
