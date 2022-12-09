2acc6f5b4df27a7a03bed47db5a0a1578df5c36c
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-08 20:19:31 -0700

    Assign class name to products container and style with grid

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { ProductsContext } from '../../contexts/products.context';
 
+import './shop.styles.scss';
+
 import ProductCard from '../../components/product-card/product-card.component';
 
 const Shop = () => {
@@ -8,7 +10,7 @@ const Shop = () => {
   const { products } = useContext(ProductsContext);
 
   return (
-    <div>
+    <div className="products-container">
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
