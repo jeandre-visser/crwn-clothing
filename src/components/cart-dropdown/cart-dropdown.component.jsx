f20754b97105ab4582cc99a8a6cb50e63d142dda
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-26 19:48:34 -0700

    Map over cartItems from CartContext

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,19 @@
+import React, { useContext } from 'react'
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
-
-import React from 'react'
+import CartItem from '../cart-item/cart-item.component';
+import { CartContext } from '../../contexts/cart.context';
 
 const CartDropdown = () => {
+
+  const { cartItems } = useContext(CartContext)
+
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items' />
+      <div className='cart-items'>
+        {cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)}
+      </div>
+
         <Button>Checkout</Button>
     </div>
   )
