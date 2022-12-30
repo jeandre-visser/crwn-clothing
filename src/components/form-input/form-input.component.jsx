e85fabb740f8cb1ec3711adcbb76489b2ca3d4ff
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-30 16:47:10 -0700

    Styled components for form input

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,21 +1,16 @@
-import './form-input.styles.scss';
+import { Group, FormInputLabel, Input } from "./form-input.styles";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input 
-        className="form-input"
-        {...otherProps}
-      />
-      {label && 
-        <label 
-          className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`} 
-        >
+    <Group>
+      <Input {...otherProps} />
+      {label && (
+        <FormInputLabel shrink={otherProps.value.length}>
           {label}
-        </label>
-      }
-    </div>
-  )
-}
+        </FormInputLabel>
+      )}
+    </Group>
+  );
+};
 
-export default FormInput
+export default FormInput;
