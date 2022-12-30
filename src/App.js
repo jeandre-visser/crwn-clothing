4ae91c5af8f3063a38b69eac3639869e5b845e49
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-30 14:42:29 -0700

    Route to a categories preview page from the shop route

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Navbar />}>
         <Route index element={<Home />}/>
-        <Route path='shop' element={<Shop />}/>
+        <Route path='shop/*' element={<Shop />}/>
         <Route path='auth' element={<Auth />}/>
         <Route path='checkout' element={<Checkout />}/>
       </Route>
