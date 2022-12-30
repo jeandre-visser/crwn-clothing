dc3c6b65efd77c2ac819d6cdb551fd357be498de
Jeandre Visser <jeandrev1414@gmail.com>
2022-12-30 16:16:54 -0700

    Use styled components for cart dropdown and import buttons to embed inside cart dropdown container

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
-import './cart-dropdown.styles.scss';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../../contexts/cart.context';
@@ -14,19 +14,19 @@ const CartDropdown = () => {
   }
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
+    <CartDropdownContainer>
+      <CartItems>
       {cartItems.length ? (
         cartItems.map((item) => (
           <CartItem key={item.id} cartItem={item} />
         ))
       ) : (
-        <span className='empty-message'>Your cart is empty</span>
+        <EmptyMessage>Your cart is empty</EmptyMessage>
       )}
-      </div>
+      </CartItems>
 
         <Button onClick={goToCheckoutHandler}>Checkout</Button>
-    </div>
+    </CartDropdownContainer>
   )
 }
 
