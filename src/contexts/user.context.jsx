a8b618de533253e18c7b0bb7f1d19ee09ea6c1e2
Jeandre Visser <jeandrev1414@gmail.com>
2023-01-02 17:37:53 -0700

    Begin migrating to redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -11,29 +11,7 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: "SET_CURRENT_USER",
-};
 
-const userReducer = (state, action) => {
-  console.log("dispatched");
-  console.log(action);
-
-  const { type, payload } = action;
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return {
-        ...state,
-        currentUser: payload,
-      };
-    default:
-      throw new Error(`Unhandled type ${type} in userReducer`);
-  }
-};
-
-const INITIAL_STATE = {
-  currentUser: null,
-};
 
 export const UserProvider = ({ children }) => {
   const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
---
+++ src/store/store.js
@@ -0,0 +1,4 @@
+import { compose, legacy_createStore as createStore, applyMiddlware} from 'redux';
+import logger from 'redux-logger';
+
+
