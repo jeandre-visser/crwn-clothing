66fa48b17a854aa6d54eb8d17517a44209f79599
Jeandre Visser <jeandrev1414@gmail.com>
2023-01-07 17:21:07 -0700

    Add a loading spinner

--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,32 @@
-import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { rootReducer } from './root-reducer';
-
-const loggedMiddleware = (store) => (next) => (action) => {
-  if(!action.type) {
-    return next(action);
-  }
-
-  console.log('type', action.type)
-  console.log('payload', action.payload)
-  console.log('currentState', store.getState())
-  next(action);
-  console.log('next state', store.getState())
-}
+import {
+  compose,
+  legacy_createStore as createStore,
+  applyMiddleware,
+} from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+import thunk from 'redux-thunk';
+import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
-  key: 'root',
- storage,
-  blacklist: ['user']
-}
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggedMiddleware];
+const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(
+  Boolean
+);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
 
 export const persistor = persistStore(store);
