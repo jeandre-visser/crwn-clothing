cc1d806a598a6cdd839f51f00186af942cc071fd
Jeandre Visser <jeandrev1414@gmail.com>
2023-01-16 20:50:15 -0700

    Stripe function setup

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./checkout.styles.scss";
 import CheckoutItem from "../checkout-item/checkout-item.component";
+import PaymentForm from "../payment-form/payment-form.component";
 
 import { useSelector } from "react-redux";
 import {
@@ -35,6 +36,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <span className="total">Total: ${cartTotal}</span>
+      <PaymentForm />
     </div>
   );
 };
