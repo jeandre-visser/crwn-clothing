2f2957f995e18a30d45574660b79be6c1a6072d5
Jeandre Visser <jeandre@nutrimeals.ca>
2023-01-21 16:35:52 -0700

    Convert cart to typescript

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,33 @@
+import { setCartItems, setIsCartOpen } from './cart.action';
+import { AnyAction } from 'redux';
+import { CartItem } from './cart.types';
+
+export type CartState = {
+  isCartOpen: boolean;
+  cartItems: CartItem[];
+};
+
+export const CART_INITIAL_STATE: CartState = {
+  isCartOpen: false,
+  cartItems: [],
+};
+
+export const cartReducer = (
+  state = CART_INITIAL_STATE,
+  action: AnyAction
+): CartState => {
+  if (setIsCartOpen.match(action)) {
+    return {
+      ...state,
+      isCartOpen: action.payload,
+    };
+  }
+
+  if (setCartItems.match(action)) {
+    return {
+      ...state,
+      cartItems: action.payload,
+    };
+  }
+  return state;
+};
