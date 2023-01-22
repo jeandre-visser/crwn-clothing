0b00e384ff8ca784fc859dce8c19b9930bed7ad7
Jeandre Visser <jeandre@nutrimeals.ca>
2023-01-22 16:33:45 -0700

    Type payment form

--- src/routes/category/category.component.tsx
+++ src/routes/category/category.component.tsx
@@ -1,16 +1,22 @@
-import { CategoryContainer, Title } from "./category.styles";
-import React, { useState, useEffect, Fragment } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { CategoryContainer, Title } from './category.styles';
+import React, { useState, useEffect, Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import {
   selectCategoriesIsLoading,
   selectCategoriesMap,
-} from "../../store/categories/category.selector";
-import ProductCard from "../../components/product-card/product-card.component";
-import Spinner from "../../components/spinner/spinner.component";
+} from '../../store/categories/category.selector';
+import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
+
+type CategoryRouteParams = {
+  category: string;
+};
 
 const Category = () => {
-  const { category } = useParams();
+  const { category } = useParams<
+    keyof CategoryRouteParams
+  >() as CategoryRouteParams;
   const categoriesMap = useSelector(selectCategoriesMap);
   const isLoading = useSelector(selectCategoriesIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
